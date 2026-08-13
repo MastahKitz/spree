9f9a6c97d86eff501b671384fe26ab921301d3bd
kitz wang <mastahkitz@gmail.com>
2026-08-14 01:15:29 +0800

    added credentials to github secrets & variables added workflow yml

--- tests/functional/config/credentials.ts
+++ tests/functional/config/credentials.ts
@@ -1,15 +1,13 @@
 export interface UserCredentials {
   email: string;
   password: string;
-  name?: string;
+  name: string;
 }
-
-// to do: migrate to jenkins or github actions, please use your own credentials here for local run
 
 export const credentials = {
   registeredUser: {
-    email: 'mastahkitz@gmail.com',
-    password: 'Test123',
-    name: 'kitz wang',
+    email: process.env.QA_EMAIL || 'mastahkitz@gmail.com',
+    password: process.env.QA_PASSWORD || 'Test123',
+    name: process.env.QA_NAME || 'kitz wang',
   } as UserCredentials,
 };
