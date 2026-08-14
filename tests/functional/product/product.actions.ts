53da2bf8c6848ac9ce48e12d9558ae4dc33ebbbf
kitz wang <mastahkitz@gmail.com>
2026-08-14 10:54:10 +0800

    added 2s wait again

--- tests/functional/product/product.actions.ts
+++ tests/functional/product/product.actions.ts
@@ -3,6 +3,7 @@ import { Page } from '@playwright/test';
 export async function clickAllProductsLink(page: Page) {
   await page.getByRole('dialog').getByRole('link', { name: 'All Products', exact: true }).click();
   await waitForPageHeading(page, 'All Products');
+  await page.waitForTimeout(2000);  // temporary fix for product load flakiness
 }
 
 export async function clickProduct(page: Page, name: string) {
@@ -21,6 +22,7 @@ export async function clickCategoryMenuItem(page: Page, category: string) {
 export async function clickSubcategoryMenuItem(page: Page, subcategory: string) {
   await page.getByRole('dialog').getByRole('link', { name: subcategory, exact: true }).click();
   await waitForPageHeading(page, subcategory);
+  await page.waitForTimeout(2000);  // temporary fix for product load flakiness
 }
 
 async function waitForPageHeading(page: Page, name: string) {
